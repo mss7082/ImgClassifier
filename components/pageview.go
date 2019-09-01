64fade44438f7477d4940974547fa5780c1225bd
Sokabi <mosokabi@9801a7895d93.ant.amazon.com>
2019-09-01 17:00:09 +0100

    Vecty - Classifier In progress

--- components/pageview.go
+++ components/pageview.go
@@ -1,12 +1,117 @@
 package components
 
 import (
+	"imagepredict/classify"
+
 	"github.com/gopherjs/vecty"
+	"github.com/gopherjs/vecty/elem"
+	"github.com/gopherjs/vecty/prop"
 )
 
 // PageView is a vecty.Component which represents the entire page.
 type PageView struct {
 	vecty.Core
+	Labels []classify.Label
+}
 
-	// Label []*classifier.Label
+// Render implements the vecty.Component interface.
+func (p *PageView) Render() vecty.ComponentOrHTML {
+	return elem.Body(
+		elem.Section(
+			vecty.Markup(
+				vecty.Class("ImgClassifierapp"),
+			),
+
+			p.renderHeader(),
+			vecty.If(len(p.Labels) > 0,
+				p.renderItemList(),
+				p.renderFooter(),
+			),
+		),
+
+		p.renderInfo(),
+	)
+}
+
+func (p *PageView) renderHeader() *vecty.HTML {
+	return elem.Header(
+		vecty.Markup(vecty.Class("header")),
+		elem.Heading1(vecty.Text("todos")),
+		elem.Input(vecty.Markup(vecty.Class("new-todo"), prop.Type("file"))),
+		elem.Image(
+			vecty.Markup(
+				vecty.Class("image"),
+				prop.Src("https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg"),
+				prop.Alt("Puppy"),
+			),
+		),
+	)
+}
+
+func (p *PageView) renderFooter() *vecty.HTML {
+	return elem.Footer(
+		vecty.Markup(
+			vecty.Class("footer"),
+		),
+
+		elem.Span(
+			vecty.Markup(
+				vecty.Class("todo-count"),
+			),
+
+			elem.Strong(
+				vecty.Text("Strong"),
+			),
+			vecty.Text("Remain Text"),
+		),
+	)
+}
+
+func (p *PageView) renderInfo() *vecty.HTML {
+	return elem.Footer(
+		vecty.Markup(
+			vecty.Class("info"),
+		),
+
+		elem.Paragraph(
+			vecty.Text("Double-click to edit a todo"),
+		),
+		elem.Paragraph(
+			vecty.Text("Created by "),
+			elem.Anchor(
+				vecty.Markup(
+					prop.Href("http://github.com/neelance"),
+				),
+				vecty.Text("Richard Musiol"),
+			),
+		),
+		elem.Paragraph(
+			vecty.Text("Part of "),
+			elem.Anchor(
+				vecty.Markup(
+					prop.Href("http://todomvc.com"),
+				),
+				vecty.Text("TodoMVC"),
+			),
+		),
+	)
+}
+
+func (p *PageView) renderItemList() *vecty.HTML {
+	var items vecty.List
+	for _, l := range p.Labels {
+		items = append(items, vecty.Text(l.Label))
+	}
+
+	return elem.Section(
+		vecty.Markup(
+			vecty.Class("main"),
+		),
+		elem.UnorderedList(
+			vecty.Markup(
+				vecty.Class("todo-list"),
+			),
+			items,
+		),
+	)
 }
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"imagepredict/components"
 
-	"imagepredict/classify"
+	"github.com/gopherjs/vecty"
 )
 
 func main() {
-	prediction := classify.Predict("https://hillrag.com/wp-content/uploads/2017/11/rabbit-pic.jpg")
-	for _, l := range prediction {
-		fmt.Printf("label: %s, probability: %.2f%%\n", l.Label, l.Probability*100)
+	// prediction := classify.Predict("https://hillrag.com/wp-content/uploads/2017/11/rabbit-pic.jpg")
+	// for _, l := range prediction {
+	// 	fmt.Printf("label: %s, probability: %.2f%%\n", l.Label, l.Probability*100)
+	// }
+
+	vecty.SetTitle("Image Classifier | Vecty")
+	p := &components.PageView{
+		// Labels: prediction,
 	}
+	vecty.RenderBody(p)
 }
